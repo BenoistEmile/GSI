fb72baccf6d9022cbf02da309420f53e550dc54e
Abrthr <88678748+Abrthr@users.noreply.github.com>
2024-04-29 16:10:27 +0200

    improve analysis

--- GSI/python/Results_Analysis.py
+++ GSI/python/Results_Analysis.py
@@ -1,32 +1,54 @@
 # %%
 from pathlib import Path
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # %%
 root_dir = Path.cwd().parent
 data_dir = root_dir / "data"
 sol_dir = root_dir / "solution"
-
 # %%
+N_prot = 25008
 ref_df = pd.read_csv(data_dir / 'YEAST-Data-NonNormalized.csv', sep = ";", index_col = 0)
 sol_df = [pd.read_csv(sol_dir / ("results_yeast0." + str(i) + "00000.csv"), index_col = 0) for i in range(1,10)]
+# %%
+ref_series = ref_df['110714_yeast_ups1_2fmol_r3']
+sol_series = sol_df[0]['abundance']
 # %%
 def str_to_float(string : str) -> float:
     return float(string.replace(',', '.'))
 
-ref_df['110714_yeast_ups1_2fmol_r3'] = ref_df['110714_yeast_ups1_2fmol_r3'].apply(str_to_float)
-
+ref_series = ref_series.apply(str_to_float)
 # %%
-sorted_ref = ref_df.sort_values('110714_yeast_ups1_2fmol_r3', ascending = False)['110714_yeast_ups1_2fmol_r3']
-sorted_sol = [df.sort_values('abundance', ascending = False) for df in sol_df]
+plt.hist(sol_series.to_numpy(), bins = np.logspace(np.log10(1), np.log10(9), 50))
 # %%
-missed_lines = [sorted_ref.index.get_loc(df.iloc[-1].name) - df['abundance'].size for df in sorted_sol]
+sorted_ref = ref_series.sort_values(ascending = False)
+sorted_sol = sol_series.sort_values(ascending = False)
 # %%
-pears_corr = [ref_df['110714_yeast_ups1_2fmol_r3'].corr(df['abundance'], 'pearson') for df in sol_df]
-spear_corr = [ref_df['110714_yeast_ups1_2fmol_r3'].corr(df['abundance'], 'spearman') for df in sol_df]
+pears_corr = [ref_series.corr(df['abundance'], 'pearson') for df in sol_df]
+spear_corr = [ref_series.corr(df['abundance'], 'spearman') for df in sol_df]
 # %%
-sol_df[0].join(sol_df[1], how = 'inner', rsuffix='2')
+sol_df[0].join(sol_df[-1], how = 'inner', rsuffix='2')
 # %%
 for i in range(9):
     print(len(ref_df.join(sol_df[i], how = 'inner')))
+# %% Stats
+TP = len(pd.merge(ref_series, sol_series, left_index = True, right_index = True, how = 'inner'))
+FP = len(sol_series) - TP
+FN = len(ref_series) - TP
+TN = N_prot - TP - FP - FN
+
+accuracy = (TP + TN) / N_prot
+specificity = TN/ (TN + FN)
+sensitivity = TP / (TP + FP)
+print(f"""True positives : {TP}
+True negatives : {TN}
+False positives : {FP}
+False negatives : {FN}
+Accuracy : {round(accuracy, 3)}
+Sensitivity : {round(sensitivity, 3)}
+Specificity : {round(specificity, 3)}""")
+# %%
+missed_lines = [sorted_ref.index.get_loc(df.iloc[-1].name) - df['abundance'].size for df in sorted_sol]
 # %%
