a5b4bece29dc0a6f2a345258cdf5668ee82dbd83
Abrthr <88678748+Abrthr@users.noreply.github.com>
2024-04-30 15:06:38 +0200

    improve python analysis functions

--- GSI/python/Results_Analysis.py
+++ GSI/python/Results_Analysis.py
@@ -11,10 +11,17 @@ sol_dir = root_dir / "solution"
 # %%
 N_prot = 25008
 ref_df = pd.read_csv(data_dir / 'YEAST-Data-NonNormalized.csv', sep = ";", index_col = 0)
-sol_df = [pd.read_csv(sol_dir / ("results_yeast0." + str(i) + "00000.csv"), index_col = 0) for i in range(1,10)]
+sol_df = [pd.read_csv(sol_dir / (f"results_yeast_10fmol0.{i}00000.csv"), index_col = 0) for i in range(1,10)]
+#%%
+N_prot = 25008
+ref_df = pd.read_csv(data_dir / 'YEAST-Data-NonNormalized.csv', sep = ";", index_col = 0)
+sol_df = pd.read_csv(sol_dir / "results_yeast_2fmol100.csv", index_col = 0)
+# %%
+ref_series = ref_df['110616_yeast_ups_10fmol']
+sol_series = sol_df[-1]['abundance']
 # %%
 ref_series = ref_df['110714_yeast_ups1_2fmol_r3']
-sol_series = sol_df[0]['abundance']
+sol_series = sol_df['abundance']
 # %%
 def str_to_float(string : str) -> float:
     return float(string.replace(',', '.'))
@@ -28,6 +35,9 @@ sorted_sol = sol_series.sort_values(ascending = False)
 # %%
 pears_corr = [ref_series.corr(df['abundance'], 'pearson') for df in sol_df]
 spear_corr = [ref_series.corr(df['abundance'], 'spearman') for df in sol_df]
+# %%
+pears_corr = ref_series.corr(sol_series, 'pearson')
+spear_corr = ref_series.corr(sol_series, 'spearman')
 # %%
 sol_df[0].join(sol_df[-1], how = 'inner', rsuffix='2')
 # %%
@@ -48,7 +58,9 @@ False positives : {FP}
 False negatives : {FN}
 Accuracy : {round(accuracy, 3)}
 Sensitivity : {round(sensitivity, 3)}
-Specificity : {round(specificity, 3)}""")
+Specificity : {round(specificity, 3)}
+Pearson correlation : {round(pears_corr, 3)}
+Spearman correlation : {round(spear_corr, 3)}""")
 # %%
 missed_lines = [sorted_ref.index.get_loc(df.iloc[-1].name) - df['abundance'].size for df in sorted_sol]
 # %%
