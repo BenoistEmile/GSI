e309b556ddb77ed4fe5264fe1a5430ec22334030
Abrthr <88678748+Abrthr@users.noreply.github.com>
2024-05-15 09:05:19 +0200

    add functions to python script

--- GSI/python/Results_Analysis2.py
+++ GSI/python/Results_Analysis2.py
@@ -1,5 +1,7 @@
+#%%
 from pathlib import Path
 import pandas as pd
+import matplotlib.pyplot as plt
 
 root_dir = Path.cwd().parent
 data_dir = root_dir / "data"
@@ -20,8 +22,8 @@ def prediction_category(row):
 
 class Results_Analysis():
 
-    def __init__(self, upper_edges: pd.DataFrame, lower_edges: pd.DataFrame, ref: pd.DataFrame, sol: pd.DataFrame) -> None:
-        self.upper_edges = upper_edges
+    def __init__(self, upper_edges: pd.DataFrame, lower_edges: pd.DataFrame, ref: pd.DataFrame, sol: pd.DataFrame, min_proba: float = 0) -> None:
+        self.upper_edges = upper_edges.loc[upper_edges["Prob"] >= min_proba]
         self.lower_edges = lower_edges
         self.ref = ref
         self.sol = sol
@@ -34,12 +36,69 @@ class Results_Analysis():
 
     def print_stats_proteins(self) -> None:
         N_edges = len(self.upper_edges)
+        median_prob = self.upper_edges["Prob"].median()
+        max_prob = self.upper_edges["Prob"].max()
+        min_prob = self.upper_edges["Prob"].min()
+        mean_prob = self.upper_edges["Prob"].mean()
+        std_prob = self.upper_edges["Prob"].std()
+        self.upper_edges["Prob"].hist(bins = 50)
         N_prot_spectra = len(self.protein_to_spectra.dropna()["protein_id"].drop_duplicates())
+        N_path_prot_spectra = len(self.protein_to_spectra[["protein_id","Spectrum"]].dropna())
         print(f"""Number of proteins : {N_prot}
 Number_of edges : {N_edges}
+Median probability : {round(median_prob, 2)}
+Min probability : {round(min_prob, 2)}
+Max probability : {round(max_prob, 2)}
+Mean probability (std) : {round(mean_prob, 2)} ({round(std_prob, 2)})
+Number of proteins with spectra : {N_prot_spectra}
+Number of proteins-spectra paths : {N_path_prot_spectra}""")
+        fig, axs = plt.subplots(1,2, sharey = True, figsize = (20,6))
+        self.upper_edges.groupby("protein_id").count()["accession"].hist(bins = self.upper_edges.groupby("protein_id").count()["accession"].max(), ax = axs[0])
+        self.protein_to_spectra[["protein_id","Spectrum"]].dropna().groupby("protein_id").count().hist(bins = self.protein_to_spectra[["protein_id","Spectrum"]].dropna().groupby("protein_id").count()["Spectrum"].max(), ax = axs[1])
+        axs[0].set_title("Number of peptides per protein")
+        axs[0].set_xlabel("Number of peptides")
+        axs[0].set_ylabel("Number of proteins")
+        axs[1].set_title("Number of spectra par protein")
+        axs[1].set_xlabel("Number of spectra")
+        plt.show()
+    
+    def print_stats_true_proteins(self) -> None:
+        true_proteins = self.protein_to_spectra.loc[self.protein_to_spectra["protein_truth"]]
+        N_proteins = len(true_proteins["protein_id"].drop_duplicates())
+        N_spectra = len(true_proteins.dropna())
+        N_prot_spectra = len(true_proteins.dropna()["protein_id"].drop_duplicates())
+        print(f"""Number of true proteins : {N_proteins}
+Number of associated spectra : {N_spectra}
 Number of proteins with spectra : {N_prot_spectra}""")
-        self.protein_to_spectra.dropna().groupby("protein_id").count().groupby("accession").size().hist(bins = self.protein_to_spectra.dropna().groupby("protein_id").count().groupby("accession").size().max()//100)
+        fig, axs = plt.subplots(1, 2, sharey = False, figsize = (20,6))
+        true_proteins.dropna()["Score"].hist(bins = 100, ax = axs[0])
+        true_proteins.dropna()[["protein_id","Score"]].groupby("protein_id").count().hist(bins = true_proteins.dropna()[["protein_id","Score"]].groupby("protein_id").count()["Score"].max(), ax = axs[1])
+        axs[0].set_title("Scores distribution")
+        axs[0].set_xlabel("Score")
+        axs[0].set_ylabel("Number of spectra")
+        axs[1].set_title("Number of spectra par protein")
+        axs[1].set_xlabel("Number of spectra")
+        axs[1].set_ylabel("Number of proteins")
+        plt.show()
 
+    def print_stats_false_proteins(self) -> None:
+        false_proteins = self.protein_to_spectra.loc[self.protein_to_spectra["protein_truth"] == False]
+        N_proteins = len(false_proteins["protein_id"].drop_duplicates())
+        N_spectra = len(false_proteins.dropna())
+        N_prot_spectra = len(false_proteins.dropna()["protein_id"].drop_duplicates())
+        print(f"""Number of false proteins : {N_proteins}
+Number of associated spectra : {N_spectra}
+Number of proteins with spectra : {N_prot_spectra}""")
+        fig, axs = plt.subplots(1, 2, sharey = False, figsize = (20,6))
+        false_proteins.dropna()["Score"].hist(bins = 100, ax = axs[0])
+        false_proteins.dropna()[["protein_id","Score"]].groupby("protein_id").count().hist(bins = false_proteins.dropna()[["protein_id","Score"]].groupby("protein_id").count()["Score"].max(), ax = axs[1])
+        axs[0].set_title("Scores distribution")
+        axs[0].set_xlabel("Score")
+        axs[0].set_ylabel("Number of spectra")
+        axs[1].set_title("Number of spectra par protein")
+        axs[1].set_xlabel("Number of spectra")
+        axs[1].set_ylabel("Number of proteins")
+        plt.show()
 
     def print_stats_scores(self) -> None:
         N_edges = len(self.lower_edges)
@@ -55,7 +114,16 @@ Median score : {round(median_score,2)}
 Min score : {round(min_score,2)}
 Max score : {round(max_score,2)}
 Mean score (std) : {round(mean_score,2)} ({round(std_score,2)})""")
-        self.lower_edges.groupby("Peptide").count()["Spectrum"].hist(bins = lower_edges.groupby("Peptide").count()["Spectrum"].max())
+        fig, axs = plt.subplots(1,2, sharey = False, figsize = (20,6))
+        self.lower_edges["Score"].hist(bins = 50, ax = axs[0])
+        self.lower_edges.groupby("Peptide").count()["Spectrum"].hist(bins = self.lower_edges.groupby("Peptide").count()["Spectrum"].max(), ax = axs[1])
+        axs[0].set_title("Scores distribution")
+        axs[0].set_xlabel("Score")
+        axs[0].set_ylabel("Number of spectra")
+        axs[1].set_title("Number of spectra per peptide")
+        axs[1].set_xlabel("Number of spectra")
+        axs[1].set_ylabel("Number of peptide")
+        plt.show()
 
     def print_stats_predictions(self):
         df = self.protein_to_spectra[["accession","prediction_category"]].drop_duplicates()
@@ -80,16 +148,40 @@ Specificity : {round(specificity, 3)}
 FNR : {round(FNR, 3)}
 PPV : {round(PPV, 3)}
 NPV : {round(NPV, 3)}""")
-        
+#%%
+for threshold in [1000,3000,5000]:
+    for n_edges in [4]:
+        upper_edges = pd.read_csv(digestion_dir / "digestion_yeast+ups1_result.csv")[["accession", "protein_id", "peptide_id", "Prob"]]
+        lower_edges = pd.read_csv(sol_dir / f"lower_edges{threshold}_{n_edges}.csv")
+        N_prot = len(upper_edges.groupby("accession"))
+
+        ref = pd.read_csv(data_dir / 'YEAST-Data-NonNormalized.csv', sep = ";")
+        sol = pd.read_csv(sol_dir / f"results_yeast_10fmol{threshold}_{n_edges}.csv")
+        ref = pd.merge(ref, upper_edges[["accession","protein_id"]].drop_duplicates(), left_on = "Accession", right_on="accession", how = 'left').drop('accession', axis = 1)
+        results = Results_Analysis(upper_edges, lower_edges, ref, sol)
+
+        print(f"==============================================================\nResults for threshold {threshold} and max edges {n_edges}")
+        # results.print_stats_proteins()
+        results.print_stats_scores()
+        # results.print_stats_true_proteins()
+        # results.print_stats_false_proteins()
+        # results.print_stats_predictions()
+        print("==============================================================")
+# %%
 upper_edges = pd.read_csv(digestion_dir / "digestion_yeast+ups1_result.csv")[["accession", "protein_id", "peptide_id", "Prob"]]
-lower_edges = pd.read_csv(sol_dir / "lower_edges12_4.csv")
+lower_edges = pd.read_csv(sol_dir / f"lower_edges100_5.csv")
 N_prot = len(upper_edges.groupby("accession"))
 
 ref = pd.read_csv(data_dir / 'YEAST-Data-NonNormalized.csv', sep = ";")
-sol = pd.read_csv(sol_dir / "results_yeast_10fmol12_4.csv")
+sol = pd.read_csv(sol_dir / f"results_yeast_10fmol100_5.csv")
 ref = pd.merge(ref, upper_edges[["accession","protein_id"]].drop_duplicates(), left_on = "Accession", right_on="accession", how = 'left').drop('accession', axis = 1)
 
-results = Results_Analysis(upper_edges, lower_edges, ref, sol)
+results = Results_Analysis(upper_edges, lower_edges, ref, sol, min_proba = 0)
 
+print(f"==============================================================\nResults for threshold {threshold} and max edges {n_edges}")
+results.print_stats_proteins()
 results.print_stats_scores()
+results.print_stats_true_proteins()
+results.print_stats_false_proteins()
 results.print_stats_predictions()
+print("==============================================================")
