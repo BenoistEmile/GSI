0898cb0bbd13ad3fd272538906239c1b6baa0233
Abrthr <88678748+Abrthr@users.noreply.github.com>
2024-05-28 16:58:01 +0200

    add python script and model to predict detectability using DbyDeep

---
+++ GSI/python/DbyDeep_script.py
@@ -0,0 +1,17 @@
+import argparse
+from pathlib import Path
+import pandas as pd
+import dbydeep_model
+
+if __name__ == "__main__":
+    gsi_path = Path.cwd()
+    opt = argparse.Namespace(retrain_flag = False,
+                              data_path = gsi_path / 'data' / 'digestion' / 'digestion_file.csv',
+                              model_path = gsi_path / 'python' / 'DbyDeep.h5',
+                              save_path = (gsi_path / 'data' / 'digestion').absolute().as_posix() + '/',
+                              job_name = "output_file")
+    print(opt)
+    dbydeep_model.main(opt)
+
+    detectabilities = pd.read_csv(gsi_path / 'data' / 'digestion' / 'output_file.csv')
+
