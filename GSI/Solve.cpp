f28ccbee387e9f86037460f7dee107a14e0e39fd
Abrthr <88678748+Abrthr@users.noreply.github.com>
2024-07-24 14:15:26 +0200

    ignore proteins with abundance < 0.1

--- GSI/Solve.cpp
+++ GSI/Solve.cpp
@@ -292,7 +292,7 @@ int Model::Solve(const float psi1, const float psi2, const float Pmin, const flo
 	IloNumArray valuesQ(env);
 	cplex.getValues(Q, valuesQ);
 	for (std::size_t i = 0; i < n; ++i) {
-		if (valuesQ[i] > 0) {
+		if (valuesQ[i] >= 0.1) {
 			solution.Add_Protein(proteins_index_new_old[i], (float)valuesQ[i]); // protein_index[i] est inversé : on donne le nouvel indice (solution = créer dès le début une autre map inversée)
 		}
 	}
