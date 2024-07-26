5006514ffccd484368b7a533a553139990ea03db
Abrthr <88678748+Abrthr@users.noreply.github.com>
2024-07-26 09:04:50 +0200

    clean main

--- GSI/main.cpp
+++ GSI/main.cpp
@@ -1,4 +1,3 @@
-
 #include "Model.h"
 #include <unordered_map>
 #include <set>
@@ -51,83 +50,62 @@ int main() {
 
 	// std::ofstream output_file = model.Open_Output_File("result");
 
-	// model.Load_Proteins("uniprot_humain_moitie.fasta");
-	// model.Load_Proteins("ups1-ups2-sequences.fasta");
-	// model.Load_Proteins("c_albicans+ups.fasta");
-	// model.Load_Proteins_Accession("yeast+ups1.fasta");
-	model.Load_Proteins_Accession("Sprot_2024-02-05.fasta");
+	model.Load_Proteins_Accession("Sprot_Gallus_gallus_2021_04_13.fasta");
+	// model.Load_Proteins_Accession("Sprot_2024-02-05.fasta");
 	std::cout << "proteins loaded : " << model.Number_Of_Proteins() << std::endl;
 
-	model.Peptide_Detectability(2, 0.00, 7, 25, true); // if SpecOMS is used, L2I must be true
-	
-	// std::string digestion_file_name = "yeast_10fmol";
-	// std::cout << std::string(digestion_file_name + "_result.csv") << std::endl;
-	// model.In_Silico_Digestion(digestion_file_name);
-	// model.In_Silico_Digestion_2(digestion_file_name);
-	
-	// model.Peptide_detectability("Dby_Deep", digestion_file_name);
+	model.Peptide_Detectability(2, 0.00, 7, 25, false); // if SpecOMS is used, L2I must be true
+
 	std::cout << "Peptide detectability computed" << std::endl;
 	std::cout << "Peptides digested : " << model.Number_Of_Peptides() << std::endl;
 
-	// std::ofstream peptides_fasta(std::filesystem::current_path().generic_string() + "/peptides_yeast+ups1.fasta");
-	// for (int i = 0; i < model.Number_Of_Peptides(); i++) {
-	// 	peptides_fasta << ">" << i << std::endl << model.Get_Peptide(i).Get_Sequence() << std::endl << std::endl;
-	// }
-	// peptides_fasta.close();
-
-	// model.Build_Theoretical_Spectra();
-	// std::cout << "theoretical spectra built : " << model.Number_Of_Peptides() << std::endl;
-
-	// model.Define_Probabilities(std::string(digestion_file_name + "_result.csv"), 0.0);
-	// model.Define_Probabilities_2(std::string(digestion_file_name + "_result.csv"), 0.0);
-	// model.Define_Probabilities(0.2f);
-	// std::cout << "probabilities defined" << std::endl;
-	// std::cout << model.Get_Peptide(207160).Get_Proteins().at(6113)[0] << std::endl;
-
-	// model.Run_Tests_Sample_Data(10, 5, 5, 2, 2, "tests_results");
-
-	// std::unordered_map<std::size_t, unsigned int> sample = model.Random_Sample(100, 100, 1, 50);
-	// std::unordered_map<std::size_t, unsigned int> sample = {{1, 1}, {3, 3}, {4, 4}};
-	// model.Simulated_Sample(sample);
-	// model.Load_Spectra("110616_yeast_ups_10fmol.ms2", 60);
 	model.Load_Spectra("QX001127_OVA.mgf", 50);
+	// model.Load_Spectra("QX002755_HeLa.mgf", 50);
 	std::cout << "spectra loaded : " << model.Number_Of_Spectra() << std::endl;
 
-	// model.Compute_Score(1);
-	// model.Compute_Score_SpecOMS(0U, 99999U, 2, 2U, 7U, 2U);
-	model.Load_Scores_SpecOMS("specoms_output_OVA.csv");
+	// model.Load_Scores_SpecOMS("specoms_output_HeLa.csv");
 	// model.Load_Scores_Prospect("110618_yeast_ups_50fmol_r1_peptides.csv", 6, 25, 4);
-	// model.Load_Scores_XTandem("QX002755_Hela.csv");
+	model.Load_Scores_XTandem("Raw_identification_results.csv");
 	std::cout << "scores computed : " << model.Number_Of_Scores() << std::endl;
 
-	model.Pre_Solve(0.5);
+	// model.Pre_Solve(0.5);
 
-	// std::set<std::tuple<float, float>> psi_values = {{1, 1}, {1, 10}, {1, 100}};
-	// model.Test_Psi_Values(psi_values, output_file, "results_yeast+ups1");
-
-	std::set<std::pair<float, float>> params_set = {{1, 1}, {1, 10}, {1, 100}, {1, 1000}, {10, 1}, {100, 1}, {1000, 1}, {0, 1}, {0.2, 0.8}, {0.4, 0.6}, {0.5, 0.5}, {0.6, 0.4}, {0.8, 0.2}, {1, 0}};
+	// std::set<std::pair<float, float>> params_set = {{1, 1}, {1, 10}, {1, 100}, {1, 1000}, {10, 1}, {100, 1}, {1000, 1}, {0, 1}, {0.2, 0.8}, {0.4, 0.6}, {0.5, 0.5}, {0.6, 0.4}, {0.8, 0.2}, {1, 0}};
+	// std::set<std::pair<float, float>> params_set = {{1, 10}};
+	std::set<std::tuple<float, float, float, float>> params_set = {{1, 10, 1.0, 0.0}};
+	// std::multiset<std::tuple<float, float, float, float>> params_set = {{1, 1, 5.0, 0.0}, {1, 1, 6.0, 0.0}, {1, 1, 7.0, 0.0}, {1, 1, 8.0, 0.0}};
 	for (auto& params : params_set) {
 
-		std::cout << params.first << ", " << params.second << std::endl;
+		std::cout << std::get<0>(params) << ", " << std::get<1>(params) << ", " << std::get<2>(params) << ", " << std::get<3>(params) << std::endl;
 
 	// unsigned int count;
 	// for (float i = 0.0; i < 1.001; i += 0.1) {
-		model.Solve(params.first, params.second);
+		auto start = std::chrono::high_resolution_clock::now();
+		model.Solve(std::get<0>(params), std::get<1>(params), std::get<2>(params), std::get<3>(params));
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration_tot = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		std::cout << duration_tot << std::endl;
 	// model.Solve(1, 10);
 		std::cout << "model solved" << std::endl;
 
 		model.Print_Solution();
 
-		// std::string file_name = "HeLa_SpecOMS_2_8_0_1_10_0.00";
-		std::string file_name = "OVA_SpecOMS_1_8_0_" + fmt::format("{0:.1f}", params.first) + "_" + fmt::format("{0:.1f}", params.second) + "_0.00";
+		std::cout << model.Get_Solution().Get_Selections().size() << std::endl;
+
+	// std::string file_name = "ova_SpecOMS_test_2_2_8_0_1.0_10.0_0.00";
+		// std::string file_name = "OVA_test_2_8_0_" + fmt::format("{0:.1f}", params.first) + "_" + fmt::format("{0:.1f}", params.second) + "_0.00";
+		std::string file_name = "OVA_pep_selection_2_8_0_" + fmt::format("{0:.1f}", std::get<0>(params)) + "_" + fmt::format("{0:.1f}", std::get<1>(params)) + "_" + fmt::format("{0:.2f}", std::get<2>(params)) + "_" + fmt::format("{0:.2f}", std::get<3>(params));
+
+		model.Save_Solution("results_" + file_name, true, true, true, true, true);
 
 		std::filesystem::path file_path = std::filesystem::current_path() / "models" / ("upper_edges_" + file_name + ".csv");
 		std::ofstream upper_edges_file(file_path);
-		upper_edges_file << "accession,protein_id,peptide_id,Prob" << std::endl;
+		upper_edges_file << "accession,protein_id,peptide_id,Prob,rank" << std::endl;
 		for (int i = 0; i < model.Number_Of_Peptides(); i++) {
 			for (auto& protein : model.Get_Peptide(i).Get_Proteins()) {
-				for (auto& edge : std::get<1>(protein)) {
-					upper_edges_file << model.Get_Protein(std::get<0>(protein)).Get_Accession() << "," << model.Get_Protein(std::get<0>(protein)).Get_Id() << "," << model.Get_Peptide(i).Get_Id() << "," << edge << std::endl;
+				// for (auto& edge : std::get<1>(protein)) {
+				for (int j = 0; j < std::get<1>(protein).size(); j++) {
+					upper_edges_file << model.Get_Protein(std::get<0>(protein)).Get_Accession() << "," << model.Get_Protein(std::get<0>(protein)).Get_Id() << "," << model.Get_Peptide(i).Get_Id() << "," << std::get<1>(protein).at(j) << "," << j << std::endl;
 				}
 			}
 		}
@@ -142,10 +120,11 @@ int main() {
 		lower_edges_file.close();
 
 		// model.Save_Solution(output_file, false, false, true);
-		model.Save_Solution("results_" + file_name, true, true, true, true);
+		// model.Save_Solution("results_" + file_name, true, true, true, true);
 		model.Clear(false, false, false, false, true);
+		std::cout << "here" << std::endl;
 	}
-	// }
+
 	// count = 0;
 	// for (auto iter = model.Get_Solution().Get_abundances().begin(); iter != model.Get_Solution().Get_abundances().end(); iter++) {
 	// 	if (iter->first <= 47) {
