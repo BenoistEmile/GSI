9bcbf9f07d5603429c7278177a52569674a556bd
Abrthr <88678748+Abrthr@users.noreply.github.com>
2024-08-09 14:59:06 +0200

    Modify Score class for double edges (missed cleavages)

--- GSI/Score.cpp
+++ GSI/Score.cpp
@@ -3,24 +3,37 @@
 
 //__________________________________________________________________________________________________________
 
-Identification::Identification(const std::size_t peptide, const std::size_t spectrum) : peptide(peptide) ,spectrum(spectrum) {}
+Identification::Identification(const std::size_t spectrum) : peptides(), spectrum(spectrum) {}
+Identification::Identification(const std::vector<std::size_t>, const std::size_t spectrum) : peptides(peptides), spectrum(spectrum) {}
 
 std::ostream& operator<<(std::ostream& os, const Identification& identification)
 {
-    os << "Identification (" << identification.peptide << " ," << identification.spectrum << ")";
+    os << "Identification (";
+    for (auto peptide: identification.peptides) {
+        os << peptide << ", ";
+    }
+    os << identification.spectrum << ")";
     return os;
+}
+
+void Identification::Add_Peptide(std::size_t peptide) {
+    peptides.push_back(peptide);
 }
 
 //__________________________________________________________________________________________________________
 
-Score::Score(const std::size_t peptide, const std::size_t spectrum, double score) : Identification(peptide, spectrum) , score(score) {}
+Score::Score(const std::vector<std::size_t> peptides, const std::size_t spectrum, double score) : Identification(peptides, spectrum) , score(score) {}
 
 const Identification* Score::Get_Edge() const {
-    return new Identification(peptide, spectrum);
+    return new Identification(peptides, spectrum);
 }
 
 std::ostream& operator<<(std::ostream& os, const Score& score)
 {
-    os << "Score (" << score.peptide << " ," << score.spectrum << ") = " << score.score;
+    os << "Score (";
+    for (auto& peptide: score.peptides) {
+        os << peptide << ", ";
+    }
+    os << score.spectrum << ") = " << score.score;
     return os;
 }
--- GSI/Score.h
+++ GSI/Score.h
@@ -1,21 +1,25 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 struct Identification {
-	const std::size_t peptide;
+	std::vector<std::size_t> peptides;
 	const std::size_t spectrum;
 
-	Identification(const std::size_t peptide, const std::size_t spectrum);
+	Identification(const std::size_t spectrum);
+	Identification(const std::vector<std::size_t> peptides, const std::size_t spectrum);
 
 	friend std::ostream& operator<<(std::ostream& os, const Identification& identification);
+
+	void Add_Peptide(const std::size_t peptide);
 };
 
 
 struct Score : public Identification {
 	double score;
 
-	Score(const std::size_t peptide, const std::size_t spectrum ,double score);
+	Score(const std::vector<std::size_t> peptides, const std::size_t spectrum ,double score);
 
 	const Identification* Get_Edge() const;
 
